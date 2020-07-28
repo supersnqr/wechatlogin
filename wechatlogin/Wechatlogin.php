545ba6dc62dc06cfff1b0e3d01bc12dcf74911a2
142536-nqr <54832569+142536-nqr@users.noreply.github.com>
2020-07-28 18:57:47 +0800

    three

--- wechatlogin/Wechatlogin.php
+++ wechatlogin/Wechatlogin.php
@@ -3,17 +3,16 @@ namespace supersnqr\wechatlogin;
 
 class Wechatlogin
 {
-	private static $config;
+	private static $config = null;
+	private static $cache = null;
+	public static function config (array $options)
+    {
+       self::$config = $options;
+	   $key = md5(get_called_class() . serialize($options));
+        if (isset(self::$cache[$key])) return self::$cache[$key];
+        return self::$cache[$key] = new static($options);
+    }
 	
-	public static function config($option = null){
-		if (is_array($option)) {
-            self::$config = new DataArray($option);
-        }
-        if (self::$config instanceof DataArray) {
-            return self::$config->get();
-        }
-        return [];
-	}
 	/**
      * 静态魔术加载方法
      * @param string $name 静态类名
@@ -22,20 +21,21 @@ class Wechatlogin
      * @throws InvalidInstanceException
      */
     public static function wechat_h5($callback=''){
-    	var_dump(self::$config);exit;	
-		$appid = config('site.appid_login');
+		$appid = self::$config['appid'];
 		$callback = urlencode($callback);
 		$url = "https://open.weixin.qq.com/connect/qrconnect?appid={$appid}&redirect_uri={$callback}&response_type=code&scope=snsapi_login&state=what#wechat_redirect";
 		return $url;
     }
 	public static function wechat_pub($callback=''){
-		$appid = config('site.public_appid');
+		$appid = self::$config['appid'];
 		$callback = urlencode($callback);
 		$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appid}&redirect_uri={$callback}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
 		return $url;
     }
 	public static function callback($code){
 		if(!empty($code)){
+			$appid = self::$config['appid'];
+			$appSecret = self::$config['appsecret'];
 			$url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$appSecret."&code=".$code."&grant_type=authorization_code";
 			$jsonResult = file_get_contents($url);
 			$resultArray = json_decode($jsonResult, true);
@@ -49,10 +49,27 @@ class Wechatlogin
 			return false;
 		}
 	}
-//  public static function __callStatic($name, $arguments)
-//  {
-//      
-//
-//  }
+	//获取基础acctoken
+    public static function gettoken(){
+		//先判断access_token  wx_token 的文件存不存在 并且 有效期在2小时内
+	    $token_expires = 7000;
+	    $file = 'wx_token';
+	    if(file_exists($file) && filemtime($file)+$token_expires > time()){
+	        $token = file_get_contents($file);
+			return $token;
+	    }
+		$appid = self::$config['appid'];
+		$appSecret = self::$config['appsecret'];
+		//获取token
+		$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$appid}&secret={$appsecret}";
+		$res = json_decode($this->curl_post($url),true);
+		if(isset($res ['access_token'])){
+			$token = $res ['access_token'];
+		    file_put_contents($file,$token);
+		    return $token;
+		}else{
+			return $res['errmsg'];
+		} 
+	}
 }
 ?>
